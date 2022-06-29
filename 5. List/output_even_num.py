2c4d3422f903a257e217830f208cf63eb5c0a6db
eTonguzov <etonguzov@gmail.com>
2022-06-29 22:55:49 +0300

    Tuple

--- 5. List/output_even_num.py
+++ 5. List/output_even_num.py
@@ -1,9 +1,14 @@
-n = list(range(10, 21))
-m = []
-for i in n:
+#нужно из списка чисел создать список с четными числами и оставить в старом списке только нечетные
+
+old_list = list(range(1, 21))
+#the_old_list = old_list.copy()# or the_old_list = old_list[start:stop:step] синтаксис срезов
+the_old_list = old_list[1::2]
+new_list = []
+for i in old_list:
     if i % 2 == 0:
-        m.append(i)#добавляем в конец нового списка i
-        n.remove(i)#удаляем из списк n i-й элемент
+        new_list.append(i)#добавляем в конец нового списка i
+        old_list.remove(i)#удаляем из списк n i-й элемент
 else:
-    print(m)
-    print(n)
+    print(new_list)
+    print(old_list)
+print(the_old_list)
